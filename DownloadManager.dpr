06dfa26c7d7911f35543b177b8163fa88f72f948
Fernando Ferreira Rocha <fernandofr510@gmail.com>
2020-08-21 14:49:13 -0300

    Add datamodule connection

--- DownloadManager.dpr
+++ DownloadManager.dpr
@@ -4,13 +4,18 @@ uses
   Vcl.Forms,
   View.Main in 'View\View.Main.pas' {ViewMain},
   Service.Download in 'Service\Service.Download.pas',
-  Service.Interfaces in 'Service\Service.Interfaces.pas';
+  Service.Consts in 'Service\Service.Consts.pas',
+  DAO.Connection in 'DAO\DAO.Connection.pas' {Connection: TDataModule},
+  Service.Database in 'Service\Service.Database.pas',
+  View.DownloadHistory in 'View\View.DownloadHistory.pas' {ViewDownloadHistory},
+  Helper.Question in 'Helper\Helper.Question.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TConnection, Connection);
   Application.CreateForm(TViewMain, ViewMain);
   Application.Run;
 end.
